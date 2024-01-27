6ade014c5f33e4f44b9a0b83b729164e03ed3688
Kaushikmak <kaushikmak35@gmail.com>
2024-01-27 20:26:51 +0530

    README file added

--- imageEditor/ImageHandler.py
+++ imageEditor/ImageHandler.py
@@ -7,9 +7,11 @@ class ImageHandling():
         self.path = path
         
         
-    '''thi8s function will return image if exists to the main.py file, to get image array for tkinter to show on UI
+    '''this function will return image if exists to the main.py file, to get image array for tkinter to show on UI
     also it will return both scale down version of image to fit it into tkinter label(but we are woking with real image shape
-    here we are downsampling our image by factor of 3) and original image in form of array'''
+    here we are downsampling our image by factor of 3) and original image in form of array
+    [Later edition] I've also added a shape return type as it was required
+    '''
     def imageReturn(self):
         try:
             image = plt.imread(self.path)
@@ -17,7 +19,10 @@ class ImageHandling():
             return [image[::5,::5,:],image,shape]
         except FileNotFoundError:
             pass
-
+    '''
+        this function will crop image from top bottom right left as values given by user in UI
+        and return a original and scaledown version of image to UI
+    '''
     def imageCrop(self,leftcrop,rightcrop,topcrop,bottomcrop):
         image = self.imageReturn()[1]
         shape = self.imageReturn()[2]
